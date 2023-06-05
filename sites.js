c49afe06f3fe8214bd0dd851f8588409191136c5
kaike <36178392+kaikealvesdesouza@users.noreply.github.com>
2023-06-04 23:03:25 -0300

    Add files via upload

---
+++ sites.js
@@ -0,0 +1,27 @@
+
+var sites = {
+	"amazon":{"title":"amazon",abstract:"um site de venda de produtos",url:"amazon.com.br","search":"s","params":[{"name":"k",f:"search"},{"name":"__mk_pt_BR",f:"n",v:"%C3%85M%C3%85%C5%BD%C3%95%C3%91"}],keywords:["comprar","shop"]},
+	"youtube":{"title":"youtube",abstract:"um site de videos postados por usuarios",url:"youtube.com.br","search":"results","params":[{"name":"search_query",f:"search"}],keywords:["video","streaming","noticia"]},
+  "wikipedia":{title:"wikipedia",abstract:"site de conhecimento universal",url:"wikipedia.org",search:"wiki",params:[],keywords:["significado","artigo","conceito","definição","resumo","o que é?","de onde veio?","como?","porque?"]},
+	"stackoverflow":{title:"stackoverflow",abstract:"forum de programação",url:"stackoverflow.com",search:"search",params:[{name:"q",f:"search"}],keywords:["programação","program","programmer","javascript","css","html","python","C#","PHP","pascal","ruby","asp.net"]},
+	"tvbrasil":{title:"tvbrasil",abstract:"empresa brasileira de comunicação",url:"busca.ebc.com.br",search:"nodes",params:[{name:"q",f:"search"},{name:"site_id",f:"",v:"tvbrasil"}],keywords:["noticia"]},
+	"revistapíaui":{title:"revista píaui",abstract:"site de noticias da politica",url:"piaui.folha.uol.com.br",search:"",params:[{name:"s",f:"search"}],keywords:["politica","noticia"]},
+	"ibge":{title:"ibge",abstract:"insituto brasileiro de geografia e estatistica",url:"ibge.gov.br",search:"busca.html", params:[{name:"searchword",f:"search"}], keywords:["cidade"]},
+	"portaldatransparencia":{title:"portaldatransparencia",abstract:"portal da transparencia da administração publica brasileira",url:"portaldatransparencia.gov.br",search:"busca",params:[{name:"termo",f:"search"}],keywords:["dinheiro","orçamento"]},
+	"eurasiagroup":{title:"eurasia group",abstract:"noticias de politica ao redor do mundo",url:"eurasiagroup.net",search:"global-search-results",params:[{name:"keyword",f:"search"}],keywords:["noticia","news","politica","politic"]},
+	"gzeromedia":{title:"gzero media",abstract:"o mundo em 60 segundos",url:"gzeromedia.com",search:"search",params:[{name:"q",f:"search"}],keywords:["news","noticia","world","democracy"]},
+	"congressoemfoco":{title:"congresso em foco", abstract:"noticias do congresso nacional direto de brasilia",url:"congressoemfoco.uol.com.br","search":"",params:[{name:"s",f:"search"}],keywords:["politica","noticia","brasilia","congresso","nacional"]},
+	"apublica":{title:"apublica",url:"apublica.org",abstract:"Agência de Jornalismo Investigativ",params:[{name:"s",f:"search"}],keywords:["noticia","politica"]},
+	".gov.br":{title:".gov.br",url:"gov.br",abstract:"Serviços e Informações do Brasil",search:"search",params:[{name:"SearchableText",f:"search"}],keywords:[]},
+	"eaulas.usp":{title:"eaulas.usp",url:"eaulas.usp.br",abstract:"Portal de video aulas",search:"search.action",params:[],keywords:["educacao","video","curso","faculdade","universidade"]},
+	"nasa":{title:"nasa",url:"nasa.gov",abstract:"America’s civil space program",search:"search",params:[{name:"query",f:"search"},{name:"affiliate",v:"nasa"},{name:"utf8",v:"%E2%9C%93"}],keywords:["tecnologia","space","spaço","terra","lua","video"]},
+  "flickr":{title:"flickr",url:"flickr.com",abstract:"o melhor aplicativo online de gerenciamento e compartilhamento no mundo",search:"search",params:[{name:"text",f:"search"}],keywords:["fotos","photos","imagens","images","imagem","retrato","foto"]},
+  "marvelcinematicuniverse":{title:"marvelcinematicuniverse",url:"marvelcinematicuniverse.fandom.com/wiki/",abstract:"The online marvel cinematic universe encyclopedia",search:"Special:Search",params:[{name:"query",f:"search"}],keywords:["marvel","fotos","photos","photo","foto"]},
+	"bibliotecanacional":{title:"bibliotecanacional",url:"bndigital.bn.gov.br/bibliotecas-digitais-pelo-mundo/",abstract:"",search:"",params:[],keywords:["livro","acervo","biblioteca","jornal","book"]}
+	,"institutocpfl":{title:"institutocpfl",url:"institutocpfl.org.br",abstract:"Plataforma de investimento social privado do Grupo CPFL Energia",search:"search",params:[{name:"q",f:"search"}],keywords:["filosofia","aula","café","philosophy"]}
+	,"soundcloud":{title:"soundcloud",url:"soundcloud.com",abstract:"",search:"search",params:[{name:"q",f:"search"}],keywords:["musica","music","sound"]}
+	,"aliceinwonderland.fandom":{title:"aliceinwonderland.fandom",url:"aliceinwonderland.fandom.com",abstract:"Alice in Wonderland wiki is a collaborative encyclopedia for everything related to Lewis Carroll's books and the Alice in Wonderland movie adaptations, fan art and all things related to Wonderland! Anyone can edit",search:"wiki/Special:Search",params:[{name:"query",f:"search"},{name:"scope",v:"internal"},{name:"navigationSearch",v:"true"}],keywords:["alice","wonderland","mirror","rabbit","white"]}
+	,"doceru":{title:"doceru",url:"doceru.com",abstract:"Tenha acesso a milhões de documentos gratuitos.",search:"show",params:[{name:"q",f:"search"}],keywords:["livro","pdf","documento"]}
+};
+		
+
